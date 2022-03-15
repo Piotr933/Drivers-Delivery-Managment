0d158a2ecfc52ea933e77f5ad9e1a057746cd333
Piotr <piotrzawada08@gmail.com>
2022-03-15 20:22:32 +0000

    Build project using Spring framework

---
+++ src/test/java/com/piotrzawada/driversdeliverymanagment/DriversDeliveryManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrzawada.driversdeliverymanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriversDeliveryManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
